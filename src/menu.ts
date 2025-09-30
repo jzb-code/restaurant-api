f84b5e73c2b2d06d9d3d8759fca87a7f5b0ca691
Jacek Zebrowski <jacekzebrowski1@gmail.com>
2025-09-30 12:04:21 +0200

    formatting fix

--- src/menu.ts
+++ src/menu.ts
@@ -1,100 +1,100 @@
-import {MenuItem} from './types';
+import type { MenuItem } from "./types";
 
 export const MENU: MenuItem[] = [
   {
-    "id": "pizza-margherita",
-    "name": "Pizza Margherita",
-    "description": "Klasyczna pizza z sosem pomidorowym, mozzarellą i świeżą bazylią.",
-    "price": 25
+    id: "pizza-margherita",
+    name: "Pizza Margherita",
+    description: "Klasyczna pizza z sosem pomidorowym, mozzarellą i świeżą bazylią.",
+    price: 25,
   },
   {
-    "id": "pizza-pepperoni",
-    "name": "Pizza Pepperoni",
-    "description": "Pizza z pikantnym pepperoni i podwójnym serem.",
-    "price": 32
+    id: "pizza-pepperoni",
+    name: "Pizza Pepperoni",
+    description: "Pizza z pikantnym pepperoni i podwójnym serem.",
+    price: 32,
   },
   {
-    "id": "spaghetti-carbonara",
-    "name": "Spaghetti Carbonara",
-    "description": "Makaron spaghetti z sosem jajecznym, boczkiem i parmezanem.",
-    "price": 28
+    id: "spaghetti-carbonara",
+    name: "Spaghetti Carbonara",
+    description: "Makaron spaghetti z sosem jajecznym, boczkiem i parmezanem.",
+    price: 28,
   },
   {
-    "id": "lasagna-bolognese",
-    "name": "Lasagna Bolognese",
-    "description": "Tradycyjna włoska lazania z sosem bolońskim i beszamelem.",
-    "price": 30
+    id: "lasagna-bolognese",
+    name: "Lasagna Bolognese",
+    description: "Tradycyjna włoska lazania z sosem bolońskim i beszamelem.",
+    price: 30,
   },
   {
-    "id": "burger-classic",
-    "name": "Burger Classic",
-    "description": "Soczysta wołowina, sałata, pomidor, cebula i sos burgerowy w bułce.",
-    "price": 26
+    id: "burger-classic",
+    name: "Burger Classic",
+    description: "Soczysta wołowina, sałata, pomidor, cebula i sos burgerowy w bułce.",
+    price: 26,
   },
   {
-    "id": "burger-cheesebacon",
-    "name": "Burger Cheese & Bacon",
-    "description": "Wołowina, ser cheddar, chrupiący boczek, cebula karmelizowana.",
-    "price": 34
+    id: "burger-cheesebacon",
+    name: "Burger Cheese & Bacon",
+    description: "Wołowina, ser cheddar, chrupiący boczek, cebula karmelizowana.",
+    price: 34,
   },
   {
-    "id": "salad-greek",
-    "name": "Sałatka Grecka",
-    "description": "Świeże warzywa, feta, oliwki i oliwa z oliwek.",
-    "price": 22
+    id: "salad-greek",
+    name: "Sałatka Grecka",
+    description: "Świeże warzywa, feta, oliwki i oliwa z oliwek.",
+    price: 22,
   },
   {
-    "id": "salad-cezar",
-    "name": "Sałatka Cezar",
-    "description": "Kurczak grillowany, grzanki, parmezan i sos cezar.",
-    "price": 24
+    id: "salad-cezar",
+    name: "Sałatka Cezar",
+    description: "Kurczak grillowany, grzanki, parmezan i sos cezar.",
+    price: 24,
   },
   {
-    "id": "soup-tomato",
-    "name": "Zupa pomidorowa",
-    "description": "Aromatyczna zupa pomidorowa z makaronem.",
-    "price": 14
+    id: "soup-tomato",
+    name: "Zupa pomidorowa",
+    description: "Aromatyczna zupa pomidorowa z makaronem.",
+    price: 14,
   },
   {
-    "id": "soup-mushroom",
-    "name": "Krem z pieczarek",
-    "description": "Delikatny krem z pieczarek z grzankami.",
-    "price": 16
+    id: "soup-mushroom",
+    name: "Krem z pieczarek",
+    description: "Delikatny krem z pieczarek z grzankami.",
+    price: 16,
   },
   {
-    "id": "dessert-tiramisu",
-    "name": "Tiramisu",
-    "description": "Włoski deser na bazie kawy, mascarpone i kakao.",
-    "price": 18
+    id: "dessert-tiramisu",
+    name: "Tiramisu",
+    description: "Włoski deser na bazie kawy, mascarpone i kakao.",
+    price: 18,
   },
   {
-    "id": "dessert-cheesecake",
-    "name": "Sernik nowojorski",
-    "description": "Kremowy sernik na spodzie z herbatników.",
-    "price": 20
+    id: "dessert-cheesecake",
+    name: "Sernik nowojorski",
+    description: "Kremowy sernik na spodzie z herbatników.",
+    price: 20,
   },
   {
-    "id": "drink-cola",
-    "name": "Coca-Cola",
-    "description": "Napój gazowany, 0.5l.",
-    "price": 8
+    id: "drink-cola",
+    name: "Coca-Cola",
+    description: "Napój gazowany, 0.5l.",
+    price: 8,
   },
   {
-    "id": "drink-water",
-    "name": "Woda mineralna",
-    "description": "Woda gazowana lub niegazowana, 0.5l.",
-    "price": 6
+    id: "drink-water",
+    name: "Woda mineralna",
+    description: "Woda gazowana lub niegazowana, 0.5l.",
+    price: 6,
   },
   {
-    "id": "drink-coffee",
-    "name": "Kawa latte",
-    "description": "Świeżo mielona kawa z mlekiem.",
-    "price": 12
+    id: "drink-coffee",
+    name: "Kawa latte",
+    description: "Świeżo mielona kawa z mlekiem.",
+    price: 12,
   },
   {
-    "id": "drink-tea",
-    "name": "Herbata Earl Grey",
-    "description": "Aromatyczna herbata czarna z nutą bergamotki.",
-    "price": 10
-  }
-]
+    id: "drink-tea",
+    name: "Herbata Earl Grey",
+    description: "Aromatyczna herbata czarna z nutą bergamotki.",
+    price: 10,
+  },
+];
