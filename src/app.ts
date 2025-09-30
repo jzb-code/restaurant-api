4877ea6250d32db9f43ed2ffd3b4efff76bc112f
Jacek Zebrowski <jacekzebrowski1@gmail.com>
2025-09-30 11:11:43 +0200

    added privacy

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import { Hono } from "hono";
 import { authMiddleware } from "./auth.js";
 import { MENU } from "./menu.js";
 import { orderRoutes } from "./routes/orderRoutes.js";
+import { serveStatic } from "@hono/node-server/serve-static";
 
 export function createApp() {
   const app = new Hono();
+
+  app.use("/privacy", serveStatic({ path: "./public/privacy.html" }));
 
   app.use("/api/*", authMiddleware());
 
